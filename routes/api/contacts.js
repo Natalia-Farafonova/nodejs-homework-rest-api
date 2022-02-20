b89036f63faf86a6fe269a0740a94c2bcca481af
Наталия Фарафонова <ferymfon1983@gmail.com>
2022-02-20 21:15:07 +0200

    дописала REST API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,56 @@
-const express = require('express')
+const express = require('express') 
+
+const contactModel = require("../../models/contacts");
+const { contactSchema } = require("./contacts-validation-schemes");
+const { validateBody } = require('../../middlewares/validation');
+
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contacts = await contactModel.listContacts()
+  res.json({
+    status: "success",
+    code: 200,
+    payload: { contacts } 
+  });
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+   const contact = await contactModel.getContactById(req.params.contactId)
+  if (contact) {
+    return res.json({ status: 'success', code: 200, payload: { contact } })
+  }
+  return res
+    .status(404)
+    .json({ status: 'error', code: 404, message: 'Not Found' })
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validateBody(contactSchema), async (req, res, next) => { 
+  const contact = await contactModel.addContact(req.body)
+  res.status(201).json({ status: 'success', code: 201, payload: { contact } })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+ const contact = await contactModel.removeContact(req.params.contactId)
+  if (contact) {
+    return res.json({ status: 'success', code: 200, payload: { contact } })
+  }
+  return res
+    .status(404)
+    .json({ status: 'error', code: 404, message: 'Not Found' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contact = await contactModel.updateContact(req.params.contactId, req.body)
+  if (contact) {
+    return res.json({ status: 'success', code: 200, payload: { contact } })
+  }
+  return res
+    .status(404)
+    .json({ status: 'error', code: 404, message: 'Not Found' })
+}) 
+
+
 
 module.exports = router
